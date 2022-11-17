bc010d13c1b8889f5fda95e1c215fb98222b9d52
RedMagicBlue <103950482+RedMagicBlue@users.noreply.github.com>
2022-11-17 22:52:31 +0100

    Delete sound.js

--- ntns/assets/sound.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("click", function () {
-  document.querySelector("a-text").setAttribute("visible", false);
-});
-
-function playing(index) {
-  console.log("now playing", index);
-  document.querySelector("#player").setAttribute("visible", true);
-}
